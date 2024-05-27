7f16e24b05eb40f7f09a48ac1bc22d0097604089
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-05-27 22:13:16 +0530

    ok

--- versions.tf
+++ versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    # Update these to reflect the actual requirements of your module
-    #    random = {
-    #      source  = "hashicorp/random"
-    #      version = ">= 2.2"
-    #    }
+    google = {
+      source = "hashicorp/google"
+      version = "5.30.0"
+    }
   }
 }
+

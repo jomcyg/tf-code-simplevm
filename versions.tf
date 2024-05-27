615e11684aee5514572b153656a302bdccbf9eb8
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-05-27 22:47:31 +0530

    ok

--- versions.tf
+++ versions.tf
@@ -1,16 +1,16 @@
 terraform {
   required_version = ">= 1.0"
 
-  # required_providers {
-  #   google = {
-  #     source = "hashicorp/google"
-  #     version = "5.30.0"
-  #   }
-  #   google-beta = {
-  #     source  = "hashicorp/google-beta"
-  #     version = "~> 5.30" # Align with google provider version (approximately)
-  #   }
-  # }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.30.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.30" # Align with google provider version (approximately)
+    }
+  }
 }
 
 provider "google-beta" {
